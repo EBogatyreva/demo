839b1d7e06dc1fb8e8da0b541bc454dd39516d40
L <EBogatyrevaPK@yandex.ru>
2022-03-23 23:41:29 +1200

    Как правильно он должен выдавать ошибку?)

--- src/main/java/com/example/demo/HelloCalculator.java
+++ src/main/java/com/example/demo/HelloCalculator.java
@@ -2,8 +2,56 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
-
+@Service
 public class HelloCalculator implements HelloService {
+    int sum;
 
+    @Override
+    public String welcomeToCalculator() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    @Override
+    public int plus(int num1, int num2) {
+        if ((num1 == 0) || (num2 == 0)) {
+            itIsError(num1, num2);
+        } else if ((num1 != 0) && (num2 != 0)) sum = num1 + num2;
+        return sum;
+    }
+
+    @Override
+    public int minus(int num1, int num2) {
+        if ((num1 == 0) || (num2 == 0)) {
+            itIsError(num1, num2);
+        } else if ((num1 != 0) && (num2 != 0)) sum = num1 - num2;
+        return sum;
+    }
+
+    @Override
+    public int multiply(int num1, int num2) {
+        if ((num1 == 0) || (num2 == 0)) {
+            itIsError(num1, num2);
+        } else if ((num1 != 0) && (num2 != 0)) sum = num1 * num2;
+        return sum;
+    }
+
+    @Override
+    public int divide(int num1, int num2) {
+        if ((num1 == 0) || (num2 == 0)) {
+            itIsError(num1, num2);
+        } else if ((num1 != 0) && (num2 != 0)) sum = num1 / num2;
+        return sum;
+    }
+
+    @Override
+    public void itIsError(int num1, int num2) {
+        try {
+            boolean b = (num1 == 0 || num2 == 0);
+
+        } catch (ArithmeticException e) {
+            System.out.println("Введите оба параметра");
+
+        }
+
+    }
 }
-
--- src/main/java/com/example/demo/HelloService.java
+++ src/main/java/com/example/demo/HelloService.java
@@ -2,4 +2,15 @@ package com.example.demo;
 
 public interface HelloService {
 
+    String welcomeToCalculator();
+
+    int plus(int num1, int num2);
+
+    int minus(int num1, int num2);
+
+    int multiply(int num1, int num2);
+
+    int divide(int num1, int num2);
+
+    void itIsError(int num1, int num2);
 }
