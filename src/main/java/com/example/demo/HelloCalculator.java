fd6e908cf1a89d3d302f7c3a724f9b55a16816a6
L <EBogatyrevaPK@yandex.ru>
2022-03-24 19:49:51 +1200

    Я видимо вчера уставшая была очень)) про нули ну да)) я прям....)

--- src/main/java/com/example/demo/HelloCalculator.java
+++ src/main/java/com/example/demo/HelloCalculator.java
@@ -23,7 +23,7 @@ public class HelloCalculator implements HelloService {
 
     @Override
     public int multiply(int num1, int num2) {
-        return  num1 * num2;
+        return num1 * num2;
     }
 
     @Override
